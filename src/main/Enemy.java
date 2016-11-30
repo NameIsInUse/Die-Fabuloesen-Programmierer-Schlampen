42c93f5f58ba99dc183e65867521955a11da71de
LuckyTrixx <daniel.schwabe@mediaspray.de>
2016-11-30 19:07:08 +0100

    Welp

--- src/main/Enemy.java
+++ src/main/Enemy.java
@@ -1,23 +1,4 @@
-<<<<<<< HEAD:OBTowerDefense/src/obtowerdefense/Enemy.java
-package obtowerdefense;
 
-public class Enemy {
-    private int x, y;
-    
-    public Enemy(int y){
-        x = 500;
-        this.y = y;
-    }
-    
-    public void update(){
-        x--;
-    }
-    
-    public int getX(){
-    	return (int) x;
-    }
-}
-=======
 package main;
 
 public class Enemy {
@@ -36,4 +17,3 @@ public class Enemy {
     	return (int) x;
     }
 }
->>>>>>> origin/master:src/main/Enemy.java
